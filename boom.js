395e5a0f1f63cbbf6b01be0a4f5536f72e9cb119
lassipispa <lassi.pispa@live.com>
2016-04-17 15:04:46 +0300

    Second commit

---
+++ boom.js
@@ -0,0 +1,37 @@
+document.body.onmousemove = function(e){
+	e = e || window.event;
+
+	var pageX = e.pageX;
+	var pageY = e.pageY;
+		// For good old Internet Explorer:
+	if(pageX === undefined){
+		pageX = e.clientX + document.body.scrolLeft + documentElement.scrolLeft;
+		pageY = e.clientY + document.body.scrolTop + documentElement.scrolTop;
+	}
+		//
+	var x = pageX/window.innerWidth;
+	var y = pageY/window.innerHeight;
+
+	function mouseSetUp(mouse, a){
+		mouse.innerHTML = Math.ceil(100*a) + "%";
+		mouse.style.opacity = 1 - x;
+	}
+
+	function circleSetUp(circle, a) {
+		circle.style.opacity = 0 + a;
+		circle.setAttribute("r", Math.ceil(300*a*2));
+		circle.setAttribute("stroke-width",50*a);
+	}
+
+	mouseSetUp(document.getElementById('mouseX'), x);
+	mouseSetUp(document.getElementById('mouseY'), y);
+
+	circleSetUp(document.getElementById('circleX'), x);
+	circleSetUp(document.getElementById('circleX1'), x*0.5);
+	circleSetUp(document.getElementById('circleX2'), x*0.25);
+	
+	circleSetUp(document.getElementById('circleY'), y);
+	circleSetUp(document.getElementById('circleY'), y);
+	circleSetUp(document.getElementById('circleY1'), y*0.5);
+	circleSetUp(document.getElementById('circleY2'), y*0.25);
+};
